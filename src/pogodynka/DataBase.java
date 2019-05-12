29a5d541fe702cd77d0705bc8a32128b75c4fe24
Wojtek <nowwak.wojtek@gmail.com>
2019-05-12 17:40:35 +0200

    commit 2

--- src/pogodynka/DataBase.java
+++ src/pogodynka/DataBase.java
@@ -1,4 +1,63 @@
 package pogodynka;
 
+import java.sql.*;
+import java.time.LocalDateTime;
+
 public class DataBase {
+
+    private static String connectionUrl = "jdbc:sqlserver://localhost:1433;database=WeatherDB;user=test;password=123456";
+    private static String table = "WeatherTable";
+    private static Connection connection = null;
+    private static Statement statement = null;
+
+    public static void createConnection() {
+        try {
+            DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+            connection = DriverManager.getConnection(connectionUrl);
+
+        } catch (Exception except) {
+            except.printStackTrace();
+        }
+    }
+
+    public static void insertEntry(int id, DataEntry entry) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("INSERT INTO " + table + "(ID, CITY, COUNTRY, TEMPERATURE, PRESSURE) values (?, ?, ?, ?, ?)");
+            preparedStatement.setInt(1, LocalDateTime.now().hashCode());
+            preparedStatement.setString(2, entry.getCountry()  );
+            preparedStatement.setString(3, entry.getCity());
+            preparedStatement.setFloat(4,   entry.getTemperature()-273);
+            preparedStatement.setFloat(5, entry.getPressure());
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlExcept) {
+            sqlExcept.printStackTrace();
+        }
+    }
+
+    public static java.util.List<DataEntry> selectEntry() {
+        java.util.List<DataEntry> entry = new java.util.LinkedList<DataEntry>();
+        try {
+            statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM " + table);
+
+            while (results.next()) {
+
+                String country = results.getString("COUNTRY");
+                String city = results.getString("City");
+                float temperature = results.getFloat("Temperature");
+                float pressure = results.getFloat("Pressure");
+               DataEntry dentry = new DataEntry(city, country, temperature, pressure);
+                entry.add(dentry);
+            }
+            results.close();
+            statement.close();
+        } catch (SQLException sqlExcept) {
+            sqlExcept.printStackTrace();
+        }
+
+        return entry;
+    }
+
+
 }
--- src/pogodynka/Pogodynka.java
+++ src/pogodynka/Pogodynka.java
@@ -12,16 +12,24 @@ import javax.xml.parsers.SAXParserFactory;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Pogodynka extends JFrame{
+
+public class Pogodynka extends JFrame {
 
 
-
-    private static String wurl = "http://api.openweathermap.org/data/2.5/weather?q=Wroclaw&mode=xml&apikey=b98d901cc8d2e7d3c14c7e071e309871";
-
-
+    private static String URLcity;
+    private static String wurl;
+    private static DataSet dataSet = new DataSet();
+    void setWurl(String city){
+        wurl = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&mode=xml&apikey=b98d901cc8d2e7d3c14c7e071e309871";
+    }
+    String getWurl(){
+        return wurl;
+    }
     class MyHandler extends DefaultHandler {
         Map<String, Boolean> elements = new HashMap<String, Boolean>();
 
@@ -65,6 +73,7 @@ public class Pogodynka extends JFrame{
                 try {
                     Pogodynka okno = new Pogodynka();
                     okno.setVisible(true);
+
                 } catch (Exception e) {
                     e.printStackTrace();}
             }
@@ -76,17 +85,19 @@ public class Pogodynka extends JFrame{
     private JButton button1;
     private JButton button2;
     private JButton button3;
-    private JTextField kraj;
-    private JTextField miasto;
-    private JTextField cinienie;
-    private JTextField temperatura;
+    private JTextArea miasto1;
+    private JTextArea kraj;
+    private JTextArea miasto;
+    private JTextArea cinienie;
+    private JTextArea temperatura;
     DataEntry entry;
 
 
     public Pogodynka(){
-
+        DataBase.createConnection();
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setBounds(100,100,500,300);
+    setBounds(100,100,600,300);
+    setTitle("Pogodynka");
     panel1 = new JPanel();
     panel1.setBorder(new EmptyBorder(5,5,5,5));
     setContentPane(panel1);
@@ -94,35 +105,80 @@ public class Pogodynka extends JFrame{
 
 
     button1 = new JButton("Check weather");
-    kraj = new JTextField();
-    miasto = new JTextField();
-    temperatura = new JTextField();
-    cinienie = new JTextField();
+    button1.setPreferredSize(new Dimension(150,20));
+    kraj = new JTextArea();
+    kraj.setPreferredSize(new Dimension(150,280));
+    miasto = new JTextArea();
+    miasto.setPreferredSize(new Dimension(150,280));
+    temperatura = new JTextArea();
+    temperatura.setPreferredSize(new Dimension(150,280));
+    cinienie = new JTextArea();
+    cinienie.setPreferredSize(new Dimension(150,280));
     WeatherTable = new JTable();
+        miasto1 = new JTextArea();
+        miasto1.setPreferredSize(new Dimension(150,20));
     button1.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-        entry = getNewWeatherDataFromUrl();
-        miasto.setText(entry.getCity());
-        kraj.setText(entry.getCountry());
-        cinienie.setText(Float.toString(entry.getPressure()));
-        temperatura.setText(Float.toString(entry.getTemperature()-273));
+        URLcity = miasto1.getText();
+        setWurl(URLcity);
+            entry = getNewWeatherDataFromUrl();
+        miasto.setText(miasto.getText()+"\n"+entry.getCity());
+        kraj.setText(kraj.getText()+"\n"+entry.getCountry());
+        cinienie.setText(cinienie.getText()+"\n"+Float.toString(entry.getPressure()));
+        temperatura.setText(temperatura.getText()+"\n"+Float.toString(entry.getTemperature()-273));
+
         }
     });
 
-    panel1.add(button1,"flowx,cell 0 1,alignx left,aligny center");
-    panel1.add(kraj,"cell 0 0 1,grow");
+    button2 = new JButton("Load DB");
+    button2.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+           java.util.List<DataEntry> loadedentry = null;
+            loadedentry = DataBase.selectEntry();
+            dataSet.eraseEntries();
 
+            for (DataEntry ent : loadedentry) {
+                dataSet.addEntry(ent);
+            }
+          }
+    });
+
+
+    button2.setPreferredSize(new Dimension(150,20));
+    button3 = new JButton("Save to DB");
+    button3.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            DataBase.insertEntry(LocalDateTime.now().hashCode(),entry);
+        }
+    });
+    button3.setPreferredSize(new Dimension(150,20));
+
+
+//    miasto1.addActionListener((ActionListener) this);
+
+
+    panel1.add(button1,"flowx,cell 0 1,alignx center,aligny center");
+    panel1.add(button2, "flowx,cell 1 1,alignx center,aligny center");
+    panel1.add(button3, "flowx,cell 2 1,alignx center,aligny center");
+    panel1.add(kraj,"cell 0 0 1,grow");
+    panel1.add(miasto1,"flowx, cell 3 1,alignx left,aligny center");
     panel1.add(miasto,"cell 1 0 1,grow");
     panel1.add(cinienie,"cell 2 0 1,grow");
     panel1.add(temperatura,"cell 3 0 1,grow");
+
+
+
     }
 
     private DataEntry getNewWeatherDataFromUrl() {
         DataEntry newEntry = new DataEntry();
         try {
             SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
-            parser.parse(wurl, new MyHandler(newEntry));
+            parser.parse(getWurl(), new MyHandler(newEntry));
 
         } catch (Exception e) {
             newEntry.setCity("Error occured");
